4a02e231d62a7d877addd49ee9f862136deaa08f
sopnendudutta <sopnendu7@gmail.com>
2026-05-12 13:37:49 +0530

    fixing the balance

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const BPercentage = document.querySelector('.Bpercentage');
 const BStatus = document.querySelector('.Bstatus');
 const BLiquid = document.querySelector('.Bliquid');
 
+navigator.getBattery().then((battery))=> {
+
+    function updateBattery() {
+        /* Battery level*/
+        let level = Math.floor(battery.level * 100);
+        BPercentage.innerHTML = `${level}`;
+        BLiquid.style.height = `${level}%`;
+        BLiquid.classList.remove(
+            'gradient-color-red',
+            'gradient-color-orange',
+            'gradient-color-yellow',
+            'gradient-color-green'
+        );
+
+
+    }
+}
